4c97beb8245e5b4184317a987957f9734e1d9b1a
3eJI9 <97410441+3eJI9@users.noreply.github.com>
2022-02-10 11:58:29 -0800

    end

--- JS/Lesson4/additional.js
+++ JS/Lesson4/additional.js
@@ -35,5 +35,23 @@
 //
 //
 //
+//      3) Створити функцію яка приймає масив будь яких обєктів, та повертає масив ключів всіх обєктів
+//      EXAMPLE:
+//      [{name: '3eJI9', age: 29}, {model: 'A6'}]  ===> [ name, age, model ]
 //
+//      let person = [{name: '3eJI9', age: 29}, {model: 'A6'}];
 //
+//      function keyArray(mas) {
+//      const newArr = [];
+//      for (let i = 0; i < mas.length; i++) {
+//      if (typeof mas[i] === "object") {
+//      key_a = Object.keys(mas[i]);
+//      for (let j = 0, l = key_a.length; j < l; j++) {
+//      newArr.push(key_a[j]);
+//      }
+//      }
+//      }
+//      return newArr;
+//      }
+//      console.log(keyArray(person));
+
